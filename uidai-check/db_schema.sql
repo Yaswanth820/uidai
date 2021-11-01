6bd6d48b48051b4f8647be0762086ab0ceaf5c04
csg33k <55750562+Yaswanth820@users.noreply.github.com>
2021-11-01 18:35:02 +0530

    Update db_schema.sql

--- uidai-check/db_schema.sql
+++ uidai-check/db_schema.sql
@@ -1,1 +1,9 @@
-CREATE TABLE `uidai`.`users` ( `Id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(25) NOT NULL , `email` VARCHAR(25) NOT NULL , `password` VARCHAR(25) NOT NULL , `aadhar` INT NOT NULL , `Phone` INT NOT NULL , PRIMARY KEY (`Id`)) ENGINE = InnoDB; 
+CREATE TABLE `uidai`.`users` ( 
+  `Id` INT NOT NULL AUTO_INCREMENT , 
+  `username` VARCHAR(25) NOT NULL , 
+  `email` VARCHAR(25) NOT NULL , 
+  `password` VARCHAR(25) NOT NULL , 
+  `aadhar` BIGINT NOT NULL , 
+  `Phone` INT NOT NULL , 
+  PRIMARY KEY (`Id`)
+) ENGINE = InnoDB; 
